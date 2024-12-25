77fbe70b16c24bdcd626f1178ee097b4afadf3d4
Ninja11 <66002184+NinjaMorph11@users.noreply.github.com>
2024-12-25 12:27:07 +0800

    Add Index.php file

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+echo 'I am typing <br>';
+echo 'I am testing';
+// This is comment
+/* This 
+is multiline 
+comment
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <button>Order a Pizza</button>
+</body>
+
+</html>
